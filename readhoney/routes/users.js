df443c87252542e04cea9e0a1074e82ea478dd44
Shakalee14 <shakalee14@gmail.com>
2016-08-31 14:34:58 -0700

    Removed unneeded logout

--- readhoney/routes/users.js
+++ readhoney/routes/users.js
@@ -37,16 +37,4 @@ router.post ( '/signup', (request, response, next) => {
     })
 })
 
-
-
-router.get('/logout', (request, response) => {
-    request.logout();
-    delete request.session;
-    response.render('/');
-    
-  });
-
-  // response.render('/logoutpage')
-
-
 module.exports = router;
